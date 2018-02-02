8ba1e641db47a72da7a7beba5af3a73565d7cf57
Emiliano Cervantes <ovman96@hotmail.com>
2018-02-02 04:56:10 -0600

    Splash screen change to main

--- app/src/main/java/com/example/emilianocervantes/animalwhistler/SplashActivity.java
+++ app/src/main/java/com/example/emilianocervantes/animalwhistler/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.example.emilianocervantes.animalwhistler;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
 
@@ -9,5 +11,13 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
