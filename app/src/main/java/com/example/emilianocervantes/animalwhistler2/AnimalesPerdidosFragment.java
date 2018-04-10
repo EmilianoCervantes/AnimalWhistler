cb2b7b7690b5a7496fbc549c945e4d7cf9e9208d
Alfredo Puente <alfredopv95@gmail.com>
2018-04-10 15:18:40 -0500

    Ya está mejorado el mapa y cambié los iconos

--- app/src/main/java/com/example/emilianocervantes/animalwhistler2/AnimalesPerdidosFragment.java
+++ app/src/main/java/com/example/emilianocervantes/animalwhistler2/AnimalesPerdidosFragment.java
@@ -10,6 +10,7 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -26,6 +27,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class AnimalesPerdidosFragment extends Fragment implements OnMapReadyCallback {
 
     private final int REQUEST_LOCATION_CODE = 99;
+    private Button acercar;
+    private Button alejar;
 
     GoogleMap map;
     private GoogleApiClient googleApiClient;
@@ -40,6 +43,22 @@ public class AnimalesPerdidosFragment extends Fragment implements OnMapReadyCall
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_animales_perdidos, container, false);
+        acercar = (Button)view.findViewById(R.id.acercar);
+        alejar = (Button)view.findViewById(R.id.alejar);
+
+        acercar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                map.animateCamera(CameraUpdateFactory.zoomIn());
+            }
+        });
+
+        alejar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                map.animateCamera(CameraUpdateFactory.zoomOut());
+            }
+        });
 
 
 
@@ -58,23 +77,28 @@ public class AnimalesPerdidosFragment extends Fragment implements OnMapReadyCall
         map = googleMap;
 
 
-
-        LatLng latLng = new LatLng(19.4284700,  -99.1276600);
-        MarkerOptions options = new MarkerOptions();
-        options.position(latLng).title("CDMX");
-        map.addMarker(options);
-        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        LatLng perrera1 = new LatLng(19.3353482,  -99.12309549999998);
+        LatLng perrera2 = new LatLng(19.2824725,  -99.0505124);
+        LatLng perrera3 = new LatLng(19.3016444,  -99.08675410000001);
+        LatLng perrera4 = new LatLng(19.3618761,  -99.21357769999997);
+        LatLng perrera5 = new LatLng(19.3624137,  -99.14864039999998);
+        MarkerOptions options1 = new MarkerOptions();
+        MarkerOptions options2 = new MarkerOptions();
+        MarkerOptions options3 = new MarkerOptions();
+        MarkerOptions options4 = new MarkerOptions();
+        MarkerOptions options5 = new MarkerOptions();
+        options1.position(perrera1).title("Centro de Control Canino Dr. Alfonso A. Ellini de la Garza").snippet("Telefono: 01 55 5607 4093");
+        options2.position(perrera2).title("Centro de Control Canino").snippet("Telefono: 01 55 2160 5439");
+        options3.position(perrera3).title("Brigada de Vigilancia Animal").snippet("Telefono: 01 55 5208 9898");
+        options4.position(perrera4).title("EsculCan").snippet("Telefono: 01 55 9197 4685");
+        options5.position(perrera5).title("Protectora Nacional de Animales").snippet("Telefono: 01 55 5604 2482");
+        map.addMarker(options1);
+        map.addMarker(options2);
+        map.addMarker(options3);
+        map.addMarker(options4);
+        map.addMarker(options5);
+        map.moveCamera(CameraUpdateFactory.newLatLng(perrera1));
 
     }
 
-    public void funcionesMapa(View view){
-        switch(view.getId()){
-            case R.id.acercar:
-                map.animateCamera(CameraUpdateFactory.zoomIn());
-                break;
-            case R.id.alejar:
-                map.animateCamera(CameraUpdateFactory.zoomOut());
-                break;
-        }
-    }
 }
